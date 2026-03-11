df6def62c4c0f9cafb10695fa8fa6ed7c447f27d
gilgulgamesh <101465208+gilgulgamesh@users.noreply.github.com>
2026-03-11 14:13:08 +1300

    Update KoRemote.py

--- KoRemote.py
+++ KoRemote.py
@@ -112,9 +112,10 @@ def on_release(key):
         pass
 
 def main():
-    print("Listening for space or left/right arrows. Hold to repeat. Esc to quit.")
+    print("Listening for space while focussed, or left/right arrows anywhere. Hold to repeat. Esc to quit.")
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
 
 if __name__ == "__main__":
     main()
+
